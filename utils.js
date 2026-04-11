e326df7981ea7ef559fb5d9eef809a9183ad2d7e
Phong Nguyen <phong@example.com>
2026-04-11 17:23:57 +0700

    feat: them ham tien ich cong va tru hai so

---
+++ utils.js
@@ -0,0 +1,11 @@
+// Hàm tiện ích cộng hai số
+function add(a, b) {
+  return a + b;
+}
+
+// Hàm tiện ích trừ hai số
+function subtract(a, b) {
+  return a - b;
+}
+
+module.exports = { add, subtract };
